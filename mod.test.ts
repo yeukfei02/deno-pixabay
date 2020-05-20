c7c72b02926f9701e3e9a6bc6af06fbeda1f04c0
Donald Wu <yeukfei02@gmail.com>
2020-05-20 11:24:52 +0800

    feature/create-project-structure-#1: create project structure

---
+++ mod.test.ts
@@ -0,0 +1,22 @@
+import { searchImage, searchVideo } from "./mod.ts";
+import { assert } from "./deps.ts";
+
+Deno.test("searchImage", async () => {
+  const result = await searchImage(
+    "15753767-8bbb44885683e7d79aa1c14c1",
+    "dog",
+    1,
+    3,
+  );
+  assert(result);
+});
+
+Deno.test("searchVideo", async () => {
+  const result = await searchVideo(
+    "15753767-8bbb44885683e7d79aa1c14c1",
+    "dog",
+    1,
+    3,
+  );
+  assert(result);
+});
---
+++ src/main.ts
@@ -0,0 +1,75 @@
+export async function searchImage(
+  apiKey: string,
+  query: string,
+  page: number | null,
+  perPage: number | null,
+) {
+  let result = null;
+
+  let params = {
+    key: apiKey,
+    q: query,
+    page: "1",
+    per_page: "20",
+  };
+  if (page) {
+    let obj = {
+      page: page,
+    };
+    params = Object.assign(params, obj);
+  }
+  if (perPage) {
+    let obj = {
+      per_page: perPage,
+    };
+    params = Object.assign(params, obj);
+  }
+  const queryString = new URLSearchParams(params).toString();
+
+  const response = await fetch(
+    `https://pixabay.com/api?${queryString}`,
+  );
+  if (response) {
+    result = response.json();
+  }
+
+  return result;
+}
+
+export async function searchVideo(
+  apiKey: string,
+  query: string,
+  page: number | null,
+  perPage: number | null,
+) {
+  let result = null;
+
+  let params = {
+    key: apiKey,
+    q: query,
+    page: "1",
+    per_page: "20",
+  };
+  if (page) {
+    let obj = {
+      page: page,
+    };
+    params = Object.assign(params, obj);
+  }
+  if (perPage) {
+    let obj = {
+      per_page: perPage,
+    };
+    params = Object.assign(params, obj);
+  }
+  const queryString = new URLSearchParams(params).toString();
+
+  const response = await fetch(
+    `https://pixabay.com/api?${queryString}`,
+  );
+  if (response) {
+    result = response.json();
+  }
+
+  return result;
+}
